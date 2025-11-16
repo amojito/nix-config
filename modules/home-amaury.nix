ef2d432fcf6897462bc4fee13c17e2ef2c68ef76
Amaury <amaury.jaffrain@gmail.com>
2025-11-16 15:57:50 -0800

    Add zsh and oh-my-zsh config

--- modules/home-amaury.nix
+++ modules/home-amaury.nix
@@ -28,6 +28,19 @@
       };
     };
 
-    programs.bash.enable = true;
+    programs.zsh = {
+      enable = true;
+
+      enableCompletion = true;
+      autosuggestion.enable = true;
+      syntaxHighlighting.enable = true;
+
+      oh-my-zsh = {
+        enable = true;
+        theme = "robbyrussell";
+        plugins = ["git"];
+      };
+    };
+
   };
 }
--- modules/users.nix
+++ modules/users.nix
@@ -1,9 +1,12 @@
 { config, pkgs, ... }:
 
 {
+  programs.zsh.enable = true;
+
   users.users.amaury = {
     isNormalUser = true;
     description = "Amaury";
     extraGroups = [ "networkmanager" "wheel" ];
+    shell = pkgs.zsh;
   };
 }
